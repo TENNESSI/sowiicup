5007c91606ffe99c09df3dca972adc7ca2b31ab7
TENNESSI <bratuhin-kostya99@mail.ru>
2025-03-13 17:00:29 +0400

    цкуцк
    Signed-off-by: TENNESSI <bratuhin-kostya99@mail.ru>

--- bot2.py
+++ bot2.py
@@ -4,7 +4,7 @@ import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from aiogram.types import FSInputFile, CallbackQuery
-from keyboards import StartKeyboard, GiveAddUsersKeyboard
+from keyboards import GiveAddUsersKeyboard
 from aiogram.enums import ParseMode
 import json
 from aiogram import F
@@ -23,13 +23,18 @@ neteloks_id = 1041729764
 def client_init_json() -> Client:
 	return service_account(filename='sowiicup-a7b6607dcc6b.json')
 
+client = client_init_json()
+table = client.open_by_key(table_id)
+title = table.worksheets()[0].title
+worksheet = table.worksheet(title)
+
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
 	if message.from_user.id == ADMIN_ID or message.from_user.id == ADMIN_ID_3:
 		await message.answer(f"Привет, Админ! @{message.from_user.username}", )
 	else:
 		await message.answer("Привет! Отправь свою анкету для регистрации на турнир по формату наже:\n\n1)ник с твича\n2)игровой ник\n3)ммр(как на вашем скрине)\n4)ид\n5) позиция\n6) быть кэпом да/нет\n\nНе забудь прикрепить скриншот ммр!!\n\nПРИМЕР АНКЕТЫ:")
-		await bot.send_photo(chat_id=message.from_user.id, photo=FSInputFile(f'photos/example.jpg'), caption="1)mindluwa\n2)No Mercy\n3)14506\n4)1273483740\n5)4-5pos\n6)да",)
+		await bot.send_photo(chat_id=message.from_user.id, photo=FSInputFile(f'images/example.jpg'), caption="1)mindluwa\n2)No Mercy\n3)14506\n4)1273483740\n5)4-5pos\n6)да",)
 		print(f'Пользователь @{message.from_user.username} нажал /start')
 
 @dp.message()
@@ -47,20 +52,29 @@ async def sendmsg(message: types.Message):
 				captain = message.caption.split('\n')[5][2:].strip()
 				data = [twitch, dotaname, mmr, dotaid, pos, captain, f'https://ru.dotabuff.com/players/{dotaid}', 'нет']
 				print(f'Получена анкета от @{message.from_user.username} {message.from_user.id}')
+
 				with open(f'players/{message.from_user.id}.txt', 'w', encoding="utf-8") as file:
 					file.writelines(f'{item}\n' for item in data)
 					print(f'Данные от @{message.from_user.username} {message.from_user.id} во временном файле, ожидают подтверждения')
-				await bot.send_photo(chat_id=ADMIN_ID_3, photo=message.photo[-1].file_id,
+
+				await message.bot.download(file=message.photo[-1].file_id,
+										   destination=f'images/{message.from_user.id}.jpg')
+				print(f'Фото {message.from_user.id}.jpg успешно загружено')
+
+				# await bot.send_photo(chat_id=ADMIN_ID_3, photo=message.photo[-1].file_id,
+				# 					 caption=f"1){twitch}\n2){dotaname}\n3){mmr}\n4){dotaid}\n5){pos}\n6){captain}\nhttps://ru.dotabuff.com/players/{dotaid}",
+				# 					 reply_markup=GiveAddUsersKeyboard(message.from_user.id))
+				await bot.send_photo(chat_id=ADMIN_ID_2, photo=message.photo[-1].file_id,
 									 caption=f"1){twitch}\n2){dotaname}\n3){mmr}\n4){dotaid}\n5){pos}\n6){captain}\nhttps://ru.dotabuff.com/players/{dotaid}",
 									 reply_markup=GiveAddUsersKeyboard(message.from_user.id))
-				# await bot.send_photo(chat_id=ADMIN_ID_2, photo=message.photo[-1].file_id,
+				# await bot.send_photo(chat_id=ADMIN_ID, photo=message.photo[-1].file_id,
 				# 					 caption=f"1){twitch}\n2){dotaname}\n3){mmr}\n4){dotaid}\n5){pos}\n6){captain}\nhttps://ru.dotabuff.com/players/{dotaid}",
 				# 					 reply_markup=GiveAddUsersKeyboard(message.from_user.id))
 				await message.answer('жди пока примут')
 			else:
 				await message.answer('Неверный формат анкеты, попробуй снова или пожалуйся Ане')
 		else:
-			await message.answer('Неверный формат анкеты, попробуй снова или пожалуйся Ане')
+			await message.answer('Анкета без скриншота!')
 
 @dp.callback_query(F.data.startswith('add_'))
 async def cancel(call: CallbackQuery):
@@ -69,10 +83,7 @@ async def cancel(call: CallbackQuery):
 	with open(f'players/{user_id}.txt', 'r', encoding="utf-8") as file:
 		data = file.readlines()
 		data[:] = [item.strip() for item in data]
-	client = client_init_json()
-	table = client.open_by_key(table_id)
-	title = table.worksheets()[0].title
-	worksheet = table.worksheet(title)
+
 	worksheet.append_row(data)
 	print(f'Пользователь {user_id} добавлен в гугл таблицу')
 
